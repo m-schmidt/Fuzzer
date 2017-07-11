6ef90c7123c9f30f7ec7a65dfa9f433b6dc6ac80
Michael Schmidt <github@mschmidt.me>
2017-07-11 22:52:41 +0200

    Move catchIO into Error module

--- src/Error.hs
+++ src/Error.hs
@@ -2,12 +2,25 @@ module Error where
 
 import System.Exit
 import System.IO
+import System.IO.Error
 
+
+-- |Report message to stderr
 report :: String -> IO ()
 report message = hPutStrLn stderr message
 
+-- |Report info messages and terminate the program
 exitWithInfo :: String -> IO a
 exitWithInfo message = do report message; exitWith ExitSuccess
 
+-- |Report error messages and terminate the program
 exitWithError :: String -> IO a
 exitWithError message = do report message; exitWith $ ExitFailure 1
+
+-- |Wrapper around catchIOError that prints error messages and terminates the program on error
+catchIO :: (FilePath -> IO a) -> FilePath -> IO a
+catchIO action path = catchIOError (action path) handler
+  where
+    handler e
+      | isPermissionError e   = exitWithError $ "Error: permission to access file '" ++ path ++ "' denied."
+      | otherwise             = exitWithError $ "Error: unknown error while accessing file '" ++ path ++ "'."
--- src/Main.hs
+++ src/Main.hs
@@ -6,16 +6,13 @@ import Control.Monad
 import Error
 import System.Directory
 import System.Environment
-import System.IO.Error
 
 
--- |Wrapper around catchIOError that prints error messages and terminates the program on error
-catchIO :: (FilePath -> IO a) -> FilePath -> IO a
-catchIO action path = catchIOError (action path) handler
-  where
-    handler e
-      | isPermissionError e   = exitWithError $ "Error: permission to access file '" ++ path ++ "' denied."
-      | otherwise             = exitWithError $ "Error: unknown error while accessing file '" ++ path ++ "'."
+main :: IO ()
+main = do
+  opts <- getArgs >>= commandLineOptions
+  catchIO checkTestScript "./test.sh"
+  checkWord opts
 
 
 -- |Check whether file at specified path exists and is executable
@@ -25,10 +22,3 @@ checkTestScript path = do
     unless (exists) $ exitWithError $ "Error: no test scipt '" ++ path ++ "' found in present working directory."
     permissions <- getPermissions path
     unless (executable permissions) $ exitWithError $ "Error: test scipt '" ++ path ++ "' is not executable."
-
-
-main :: IO ()
-main = do
-  opts <- getArgs >>= commandLineOptions
-  catchIO checkTestScript "./test.sh"
-  checkWord opts
