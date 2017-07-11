030e59fd4348e8804bcf6d60ac772d6d59c5a711
Michael Schmidt <github@mschmidt.me>
2017-07-11 16:53:52 +0200

    Add check that test.sh exists and is executable

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,33 @@ module Main where
 
 import Check
 import Commandline
+import Control.Monad
+import Error
+import System.Directory
 import System.Environment
+import System.IO.Error
+
+
+-- |Wrapper around catchIOError that prints error messages and terminates the program on error
+catchIO :: (FilePath -> IO a) -> FilePath -> IO a
+catchIO action path = catchIOError (action path) handler
+  where
+    handler e
+      | isPermissionError e   = exitWithError $ "Error: permission to access file '" ++ path ++ "' denied."
+      | otherwise             = exitWithError $ "Error: unknown error while accessing file '" ++ path ++ "'."
+
+
+-- |Check whether file at specified path exists and is executable
+checkTestScript :: FilePath -> IO ()
+checkTestScript path = do
+    exists <- doesPathExist path
+    unless (exists) $ exitWithError $ "Error: no test scipt '" ++ path ++ "' found in present working directory."
+    permissions <- getPermissions path
+    unless (executable permissions) $ exitWithError $ "Error: test scipt '" ++ path ++ "' is not executable."
 
 
 main :: IO ()
 main = do
   opts <- getArgs >>= commandLineOptions
+  catchIO checkTestScript "./test.sh"
   checkWord opts
