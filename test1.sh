749deed80983ea102c2ca6002c8b33ecc978f36d
Michael Schmidt <github@mschmidt.me>
2018-05-04 21:05:10 +0200

    Update comments in test scripts

--- test1.sh
+++ test1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Test script used for expression evaluation tests
 
-# The script gets one file containing the C code as parameter
+# The script gets one file containing the C code as parameter.
 INPUTFILE=$1
 
 # ...add your tests here, exit with zero/non-zero for success/failure...
