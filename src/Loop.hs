32fc911e8ce3c0308c0e671b66f6803595eab4c0
Michael Schmidt <github@mschmidt.me>
2018-05-05 15:26:14 +0200

    Add printers for loop types

--- src/Loop.hs
+++ src/Loop.hs
@@ -1,6 +1,12 @@
 module Loop
   ( Loop(..)
+  , printCounterType
+  , printCondition
+  , printConstant
   ) where
+
+import Data.ByteString.Builder
+import Data.Monoid
 
 
 -- |Specification for a loop
@@ -23,9 +29,10 @@ data LoopType
 
 
 -- |C datatypes for loop counter specifying signedness and bit width
-data CounterType
-  = Signed   Integer
-  | Unsigned Integer
+data CounterType = CounterType
+  { signed   :: Bool
+  , bitwidth :: Integer
+  }
   deriving (Eq, Show)
 
 
@@ -43,3 +50,38 @@ data ConditionType
 -- |Specification for an immediate constant
 data Constant = Constant CounterType Integer
   deriving (Eq, Show)
+
+
+-- |Printer for loop counter data type in C syntax
+printCounterType :: CounterType -> Builder
+printCounterType t = signPrefix <> baseType
+  where
+    signPrefix = string8 $ if signed t then "signed " else "unsigned "
+    baseType   = string8 $ case bitwidth t of
+                             8  -> "char"
+                             16 -> "short"
+                             32 -> "int"
+                             64 -> "long long"
+                             _  -> undefined
+
+
+-- |Printer for comparion operators
+printCondition :: ConditionType -> Builder
+printCondition c = case c of
+  LessThan     -> string8 " < "
+  LessEqual    -> string8 " <= "
+  GreaterThan  -> string8 " > "
+  GreaterEqual -> string8 " >= "
+  Equal        -> string8 " == "
+  NotEqual     -> string8 " != "
+
+
+-- |Printer for immediate constants
+printConstant :: CounterType -> Integer -> Builder
+printConstant t v = integerDec v <> signSuffix <> widthSuffix
+  where
+    signSuffix  = string8 $ if signed t then "" else "u"
+    widthSuffix = string8 $ case bitwidth t of
+                              32 -> "l"
+                              64 -> "ll"
+                              _  -> ""
