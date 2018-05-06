0470dd3ba5ad8030e180f5929aa270f0d2224d4a
Michael Schmidt <github@mschmidt.me>
2018-05-06 14:09:29 +0200

    Simplify LoopTest module

--- src/LoopTest.hs
+++ src/LoopTest.hs
@@ -38,6 +38,10 @@ testProgramPrefix :: Builder
 testProgramPrefix = string8 [str|
 #include <stdlib.h>
 
+#ifndef __COMPCERT__
+# define __builtin_ais_annot(X) do {} while(0)
+#endif
+
 void exit_ok(void)
 {
     exit(EXIT_SUCCESS);
@@ -68,69 +72,62 @@ mainSuffix = string8 [str|
 
 -- |Generate the code for a test function 'n' with a given signature
 testFunction :: Int -> Loop -> Builder
-testFunction n spec@(Loop _ ct cond (Constant cts start) (Constant cti increment) (Constant cte end) bound)
-
-  =  string8 "int __attribute__ ((noinline)) test_incr" <> intDec n <> string8 "(int x)\n"
-  <> string8 "{\n"
-  <> flowAnnot bound
-  <> string8 "    return x+1;\n\
-             \}\n\n"
-
-  <> string8 "int __attribute__ ((noinline)) test" <> intDec n <> string8 "(void)\n"
-  <> string8 "{\n"
-  <> string8 "    " <> printCounterType ct <> string8 " i;\n"
-  <> string8 "    count = 0;\n\n"
-  <> testBody spec
-  <> string8 "\n    return (count != " <> printConstant ct bound <> string8 ");\n\
-             \}\n"
-
+testFunction n (Loop lt ct cond (Constant cts start) (Constant cti increment) (Constant cte end) bound)
+  =  incrRoutine <> textRoutine
   where
+    -- routine to increment a separate iteration counter
+    incrRoutine = string8 "int __attribute__ ((noinline)) test_incr" <> intDec n <> string8 "(int x)\n{\n"
+               <> flowAnnot bound
+               <> string8 "    return x+1;\n}\n\n"
 
-    testBody (Loop Do _ _ _ _ _ _)
-      =  setupCounter start
-      <> string8 "    do\n    {\n"
-      <> loopAnnot bound
-      <> incCounter increment
-      <> updateCount
-      <> string8 "    }\n    while (" <> checkExit cond end <> string8 ");\n"
+    -- routine containing the test loop
+    textRoutine = string8 "int __attribute__ ((noinline)) test" <> intDec n <> string8 "(void)\n{\n"
+               <> string8 "    " <> printCounterType ct <> string8 " i;\n"
+               <> string8 "    count = 0;\n\n"
+               <> testBody
+               <> string8 "\n    return (count != " <> printConstant ct bound <> string8 ");\n}\n"
 
-    testBody (Loop For _ _ _ _ _ _)
-      =  string8 "    for (i = " <> printConstant ct start <> string8 "; " <> checkExit cond end <> string8 "; i += " <> printConstant ct increment <> string8 ")\n"
-      <> string8 "    {\n"
-      <> loopAnnot bound
-      <> updateCount
-      <> string8 "    }\n"
+    -- the actual test loop
+    testBody = case lt of
+      Do    -> setupCounter
+            <> string8 "    do\n    {\n"
+            <> incCounter increment
+            <> loopAnnot bound
+            <> updateCount
+            <> string8 "    }\n    while (" <> checkExit <> string8 ");\n"
+      For   -> string8 "    for (i = " <> printConstant ct start <> string8 "; " <> checkExit <> string8 "; i += " <> printConstant ct increment <> string8 ")\n    {\n"
+            <> loopAnnot bound
+            <> updateCount
+            <> string8 "    }\n"
+      While -> setupCounter
+            <> string8 "    while (" <> checkExit <> string8 ")\n    {\n"
+            <> incCounter increment
+            <> loopAnnot bound
+            <> updateCount
+            <> string8 "    }\n"
 
-    testBody (Loop While _ _ _ _ _ _)
-      =  setupCounter start
-      <> string8 "    while (" <> checkExit cond end <> string8 ")\n    {\n"
-      <> loopAnnot bound
-      <> incCounter increment
-      <> updateCount
-      <> string8 "    }\n"
-
-    -- Loop bound annottion for loop body
+    -- loop bound annottion for loop body
     loopAnnot b | b > 0      = string8 "        __builtin_ais_annot(\"loop %here bound:" <> integerDec b <> string8 ".." <> integerDec b <> string8 ";\");\n"
                 | otherwise  = string8 "        __builtin_ais_annot(\"instruction %here assert reachable: false;\");\n"
 
-    -- Setup of loop counter
-    setupCounter v           = string8 "    i = " <> cast cts <> printConstant cts v <> string8 ";\n"
+    -- setup of loop counter
+    setupCounter             = string8 "    i = " <> cast cts <> printConstant cts start <> string8 ";\n"
 
-    -- Increment of counter using positive immediates
+    -- increment of counter using positive immediates
     incCounter i | i < 0     = string8 "        i = " <> cast cti <> string8 "i - " <> printConstant cti (negate i) <> string8 ";\n"
                  | otherwise = string8 "        i = " <> cast cti <> string8 "i + " <> printConstant cti i <> string8 ";\n"
 
-    -- Check of loop exit condition
-    checkExit c v            = cast cte <> string8 "i" <> printCondition c <> cast cte <> printConstant cte v
+    -- check of loop exit condition
+    checkExit                = cast cte <> string8 "i" <> printCondition cond <> cast cte <> printConstant cte end
 
-    -- Flow annotation for increment routine
+    -- flow annotation for increment routine
     flowAnnot b | b > 0      = string8 "    __builtin_ais_annot(\"flow sum: point(%here) == "<> integerDec b <> string8 " point('main');\");\n"
                 | otherwise  = string8 "    __builtin_ais_annot(\"instruction %here assert reachable: false;\");\n"
 
-    -- Call to increment routine
+    -- call to increment routine
     updateCount              = string8 "        count = test_incr" <> intDec n <> string8 "(count);\n"
 
-    -- Typecast when target type is different
+    -- typecast when target type is different
     cast t | t /= ct         = string8 "(" <> printCounterType t <> string8 ")"
            | otherwise       = string8 ""
 
